129ca8c4862061df79c73cbcb24ad5d2359b038e
darboe <adarboe95@gmail.com>
2018-01-29 11:08:26 +0000

    d

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+
+  function test () {
+    var hideMenu  =document.getElementById('hide');
+     var x =hideMenu.style.display=='block';
+        if(x==true){
+            hideMenu.style.display='none';
+        }
+        else{
+            hideMenu.style.display='block';
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function showMenu(){
+                     //var menu = document.getElementBy('hi');
+
+                     // menu.style.background="red";
+
+      
+
+                     /*
+                     if (menu.style.display=='none') {
+                            menu.style.display='block';
+                     }else{
+
+                            menu.style.display='none';
+                     }
+                 }
+
+                 */
